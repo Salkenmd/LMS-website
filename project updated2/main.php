0f3c12784bbbc855a7ae27e205498a591cb5b829
Salkenmd <151944881+Salkenmd@users.noreply.github.com>
2024-02-24 16:22:07 +0600

    Fixed

--- project updated2/main.php
+++ project updated2/main.php
@@ -1,4 +1,5 @@
 <?php
+
     if (isset($_POST['display_books'])) {
         $host = "sql200.infinityfree.com";
         $dbusername = "if0_35176689";
@@ -8,6 +9,22 @@
         $search = $_POST['search'];
         $sql = "SELECT * FROM Book WHERE Title LIKE '%$search%'";
         $result = mysqli_query($conn, $sql);
+        echo"<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Main Page</title>
+        </head>
+        <?php 
+        include("main.php");
+        include("showallbooks.php");
+        ?>
+        <body>
+            <form action="main.php" method="post">
+                <input type="text" name="search" placeholder="Search by title...">
+                <button type="submit" name="display_books">Display Books</button>
+            </form>
+        </body>
+        </html>";
         if (mysqli_num_rows($result) > 0) {
             echo "<table border='1'>";
             echo "<tr><th>Title</th><th>Quantity</th></tr>";
