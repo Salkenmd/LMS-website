b60a4cce7b9b41b5d98ba051fd10ac99e48b1e21
Salkenmd <151944881+Salkenmd@users.noreply.github.com>
2024-02-24 16:10:26 +0600

    FUCK YEAH

--- project updated2/showallbooks.php
+++ project updated2/showallbooks.php
@@ -1,16 +1,18 @@
 <?php
-if (isset($_POST["showbook"])) {
-    // Connect to the database
 $host = "sql200.infinityfree.com";
 $dbusername = "if0_35176689";
 $dbpassword = "qQJY4USNIKZj6";
 $database = "if0_35176689_db_library";
 $conn = new mysqli($host, $dbusername, $dbpassword, $database);
-
-// Check connection
 if (!$conn) {
   die("Connection failed " . mysqli_connect_error());
 }
+if (isset($_POST["showbook"])) {
+    // Connect to the database
+
+
+// Check connection
+
 
 // Fetch all books from the Book table
 $sql = "SELECT Title, Quantity FROM Book";
@@ -27,8 +29,8 @@ if (mysqli_num_rows($result) > 0) {
 } else {
   echo "0 results";
 }
-
+}
 // Close the connection
 mysqli_close($conn);
-}
+
 ?>
