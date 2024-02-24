5c0ec316dd9e50966b251816ceb1e8ba840a75bf
Salkenmd <151944881+Salkenmd@users.noreply.github.com>
2024-02-24 16:17:40 +0600

    fixed

--- project updated2/showallbooks.php
+++ project updated2/showallbooks.php
@@ -1,31 +1,21 @@
 <?php
-    // Connect to the database
-$host = "sql200.infinityfree.com";
-$dbusername = "if0_35176689";
-$dbpassword = "qQJY4USNIKZj6";
-$database = "if0_35176689_db_library";
-$conn = new mysqli($host, $dbusername, $dbpassword, $database);
-// Check connection
-if (!$conn) {
-  die("Connection failed " . mysqli_connect_error());
-}
 if (isset($_POST["showbooks"])) {
-// Fetch all books from the Book table
-$sql = "SELECT Title, Quantity FROM Book";
-$result = mysqli_query($conn, $sql);
-
-// Display all books in a table
-if (mysqli_num_rows($result) > 0) {
-  echo "<table><tr><th>Title</th><th>Quantity</th></tr>";
-  // Output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr><td>" . $row["Title"]. "</td><td>" . $row["Quantity"]. "</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
+    $host = "sql200.infinityfree.com";
+    $dbusername = "if0_35176689";
+    $dbpassword = "qQJY4USNIKZj6";
+    $database = "if0_35176689_db_library";
+    $conn = new mysqli($host, $dbusername, $dbpassword, $database);
+    $search = $_POST['search'];
+    $sql = "SELECT * FROM Book";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table border='1'>";
+        echo "<tr><th>Title</th><th>Quantity</th></tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row['Title'] . "</td><td>" . $row['Quantity'] . "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No books found.";
+    }
 }
-}
-// Close the connection
-mysqli_close($conn);
-?>
