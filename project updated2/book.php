2a45cf3823c4d1c80682b4f40c8036303eb4c5bb
Salkenmd <151944881+Salkenmd@users.noreply.github.com>
2024-03-04 10:28:54 +0600

    try

--- project updated2/book.php
+++ project updated2/book.php
@@ -6,7 +6,7 @@ $dbusername = "if0_35176689";
 $dbpassword = "qQJY4USNIKZj6";
 $database = "if0_35176689_db_library";
 
-$bookId = isset($_POST['book_id']) ? $_POST['book_id'] : null;
+$bookTitle = isset($_SESSION['book_title']) ? $_SESSION['book_title'] : null;
 
 // Connect to the database
 $conn = new mysqli($host, $dbusername, $dbpassword, $database);
@@ -16,28 +16,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve information about the selected book based on the book ID
-if ($bookId) {
-    $sql = "SELECT Book.BookID, Book.Title, Book.ISBN, Book.AuthorID, Book.GenreID, Book.PublisherID, Book.PublicationYear, Book.Quantity, Author.AuthorName, Genre.GenreName, Publisher.PublisherName
-            FROM Book
-            INNER JOIN Author ON Book.AuthorID = Author.AuthorID
-            INNER JOIN Genre ON Book.GenreID = Genre.GenreID
-            INNER JOIN Publisher ON Book.PublisherID = Publisher.PublisherID
-            WHERE Book.BookID = ?";
-
+// Retrieve the book ID based on the book title
+if ($bookTitle) {
+    $sql = "SELECT BookID FROM Book WHERE Title = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $bookId);
+    $stmt->bind_param("s", $bookTitle);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    $book = $result->fetch_assoc();
+    $bookId = $result->fetch_assoc();
 
     // Check if the book exists and if there is enough quantity
-    if ($book && $book['Quantity'] > 0) {
+    if ($bookId && $bookId['Quantity'] > 0) {
         // Decrease the quantity of the book by 1
         $sql = "UPDATE Book SET Quantity = Quantity - 1 WHERE BookID = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("i", $bookId);
+        $stmt->bind_param("i", $bookId['BookID']);
         $stmt->execute();
 
         // Close the statement and the connection
@@ -48,8 +42,8 @@ if ($bookId) {
         exit;
     }
 } else {
-    // Handle the case when the book ID is not provided
-    header("Location: error.php?error=Book ID not provided.");
+    // Handle the case when the book title is not provided
+    header("Location: error.php?error=Book title not provided.");
     exit;
 }
 
@@ -64,16 +58,16 @@ $conn->close();
 </head>
 <body>
     <div class='book-container'>
-        <h1 class='book-title'><?php echo htmlspecialchars($book['Title']); ?></h1>
+        <h1 class='book-title'><?php echo htmlspecialchars($bookId['Title']); ?></h1>
         <div class='book-info'>
-            <p>Author: <?php echo htmlspecialchars($book['AuthorName']); ?></p>
-            <p>Year: <?php echo htmlspecialchars($book['PublicationYear']); ?></p>
-            <p>Publisher: <?php echo htmlspecialchars($book['PublisherName']); ?></p>
+            <p>Author: <?php echo htmlspecialchars($bookId['AuthorName']); ?></p>
+            <p>Year: <?php echo htmlspecialchars($bookId['PublicationYear']); ?></p>
+            <p>Publisher: <?php echo htmlspecialchars($bookId['PublisherName']); ?></p>
             <p>Username: <?php echo htmlspecialchars($_SESSION['username']); ?></p>
         </div>
         <div class='request-button-container'>
             <form action='request.php' method='post'>
-                <input type='hidden' name='book_id' value='<?php echo $book['BookID']; ?>'>
+                <input type='hidden' name='book_id' value='<?php echo $bookId['BookID']; ?>'>
                 <button type='submit' name='request_book' class='request-button'>Request</button>
             </form>
         </div>
